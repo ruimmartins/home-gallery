622fe1a4fa5af5f6965994e8434ee3d2aca4ed13
Rui Martins <ruimmartins90@gmail.com>
2023-01-18 16:40:29 +0000

    changing media navigation

--- packages/webapp/src/single/MediaNav.tsx
+++ packages/webapp/src/single/MediaNav.tsx
@@ -49,39 +49,15 @@ export const MediaNav = ({current, prev, next, listLocation, showNavigation, dis
 
   return (
     <>
-      { prev &&
-        <div className="mediaNav -left">
-          <a onClick={() => dispatch({type: 'prev'})} className={buttonClass} title="Show previous media (left arrow)">
-            <i className="fas fa-chevron-left fa-2x"></i>
-          </a>
-        </div>
-      }
-      { next &&
-        <div className="mediaNav -right">
-          <a onClick={() => dispatch({type: 'next'})} className={buttonClass} title="Show next media (right arrow)">
-            <i className="fas fa-chevron-right fa-2x"></i>
-          </a>
-        </div>
-      }
       <div className="mediaNav -bottom">
         { listLocation &&
           <a onClick={() => dispatch({type: 'list'})} className={buttonClass} title="Show media stream (ESC)">
             <i className="fas fa-th fa-2x"></i>
           </a>
         }
-        { hasGeo &&
-          <a onClick={() => dispatch({type: 'map'})} className={buttonClass} title="Show map of entry (m)">
-            <i className="fas fa-map fa-2x"></i>
-          </a>
-        }
         { current?.similarityHash &&
           <a onClick={() => dispatch({type: 'similar'})} className={buttonClass} title="Show similar images (s)">
             <i className="fas fa-seedling fa-2x"></i>
-          </a>
-        }
-        { query.type != 'none' &&
-          <a onClick={() => dispatch({type: 'chronology'})} className={buttonClass} title="Show chronology (c)">
-            <i className="fas fa-clock fa-2x"></i>
           </a>
         }
         { current &&
