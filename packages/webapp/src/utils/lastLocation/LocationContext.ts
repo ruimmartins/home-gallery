a1ef3995aba4e6f325321bed1134805f48ac0434
Rui Martins <ruimmartins90@gmail.com>
2023-05-01 12:11:30 +0100

    deleting old frontend

--- packages/webapp/src/utils/lastLocation/LocationContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-
-import { Location, DefaultLocation } from './Location'
-
-export const LocationContext = createContext<Location>(DefaultLocation)
--- packages/webapp/src/utils/lastLocation/useLastLocation.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react'
-import { LocationContext } from './LocationContext'
-
-export const useLastLocation = () => {
-  return useContext(LocationContext);
-}
